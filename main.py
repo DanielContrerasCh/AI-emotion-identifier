fe6768e1ddc113c6fb2275496ab53d37c52c4d56
Tommination <thomas.rydow@gmail.com>
2026-01-05 10:10:14 +0100

    Model works reasonably well, 0.815 precision on 0.15 test split. Alpha tuned and normalization on both redictions and training data

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from sklearn.multioutput import MultiOutputRegressor
 from sklearn.linear_model import Ridge
 from sklearn.pipeline import make_pipeline
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import GridSearchCV
+
+
 
 training_set = pd.read_csv("cleaned_amazon_reviews.csv")
 review = training_set["cleaned_review"]
@@ -14,12 +17,14 @@ emotions = training_set[emotion_names].values
 non_zero_mask = ~np.all(emotions == 0, axis=1)
 review = review[non_zero_mask]
 emotions = emotions[non_zero_mask]
+row_sums = emotions.sum(axis=1, keepdims=True)
+emotions = emotions / row_sums
 X_train, X_test, y_train, y_test = train_test_split(review, emotions, test_size=0.15, random_state=42)
 
 
 
-tfidf = TfidfVectorizer(ngram_range=(1,2), max_features=30000)
-ridge = MultiOutputRegressor(Ridge(alpha=1.0))
+tfidf = TfidfVectorizer(ngram_range=(1,2), max_features=50000)
+ridge = MultiOutputRegressor(Ridge(alpha = 0.6))
 pipe = make_pipeline(tfidf, ridge)
 pipe.fit(X_train, y_train)
 
@@ -27,12 +32,12 @@ y_pred = pipe.predict(X_test)
 y_true = np.array(y_test)
 y_pred = np.squeeze(y_pred)
 y_pred = np.clip(y_pred, 0, 1)
+y_pred = y_pred / y_pred.sum(axis=1, keepdims=True)
 
-for text, pred, true in zip(X_test, y_pred, y_true):
-    print("REVIEW:", text)
-    for name, p, t in zip(emotion_names, pred, true):
-        print(f"  {name:9s} Pred: {p:.3f}  True: {t:.3f}")
-    print()
+
+top3_accuracy = top_k_match(y_true, y_pred, k=3)
+
+
 print("MAE per emotion:", mean_absolute_error(y_test, y_pred, multioutput='raw_values'))
 print("Mean MAE:", mean_absolute_error(y_test, y_pred))
 mean_values = y_train.mean(axis=0)
@@ -41,4 +46,3 @@ baseline_pred = np.tile(mean_values, (len(y_test), 1))
 print("Baseline MAE:", mean_absolute_error(y_test, baseline_pred))
 corr = np.corrcoef(y_test.flatten(), y_pred.flatten())[0,1]
 print("Correlation:", corr)
-
